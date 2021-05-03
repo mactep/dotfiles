27e87385a9c6749dc12ce782267e2db638154496
mactep <tulio@brunoro.org>
2021-05-03 08:06:37 -0300

    Add neovim paq bootstrap command

--- .config/nvim/lua/paq.lua
+++ .config/nvim/lua/paq.lua
@@ -1,3 +1,9 @@
+local install_path = vim.fn.stdpath("data") .. "/site/pack/paqs/opt/paq-nvim"
+local fresh_install = vim.fn.empty(vim.fn.glob(install_path))
+if fresh_install > 0 then
+  vim.cmd("!git clone https://github.com/savq/paq-nvim " .. install_path)
+end
+
 vim.cmd 'packadd paq-nvim'
 local paq = require'paq-nvim'.paq
 paq{'savq/paq-nvim', opt=true}
@@ -10,3 +16,7 @@ paq 'nvim-treesitter/nvim-treesitter'
 paq 'nvim-telescope/telescope.nvim'
 paq 'nvim-lua/plenary.nvim'
 paq 'nvim-lua/popup.nvim'
+
+if fresh_install > 0 then
+  vim.cmd[[ PaqInstall ]]
+end
