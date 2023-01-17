e990884f4ca7c0d08a2c928a4652e66a53028941
mactep <tulio@brunoro.org>
2023-01-17 12:23:02 -0300

    nvim: fix telescope config warnings and add methods

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,74 +1,91 @@
 local present, telescope = pcall(require, "telescope")
-if present then
-	local fb_actions = telescope.extensions.file_browser.actions
-	telescope.setup({
-		defaults = {
-			preview = {
-				-- makes binary preview faster
-				msg_bg_fillchar = " ",
-			},
+if not present then
+	return
+end
+
+telescope.builtin = require("telescope.builtin")
+
+local fb_actions = telescope.extensions.file_browser.actions
+telescope.setup({
+	defaults = {
+		preview = {
+			-- makes binary preview faster
+			msg_bg_fillchar = " ",
 		},
-		extensions = {
-			file_browser = {
-				hijack_netrw = true,
-				initial_mode = "normal",
-				respect_gitignore = true,
-				mappings = {
-					n = {
-						["l"] = "select_default",
-						["h"] = fb_actions.goto_parent_dir,
-						["H"] = fb_actions.toggle_hidden,
-						["<Tab>"] = "toggle_selection",
-						["a"] = fb_actions.create,
-					},
+	},
+	extensions = {
+		file_browser = {
+			hijack_netrw = true,
+			initial_mode = "normal",
+			respect_gitignore = true,
+			mappings = {
+				n = {
+					["l"] = "select_default",
+					["h"] = fb_actions.goto_parent_dir,
+					["H"] = fb_actions.toggle_hidden,
+					["<Tab>"] = "toggle_selection",
+					["a"] = fb_actions.create,
 				},
 			},
 		},
-	})
+	},
+})
 
-	telescope.load_extension("fzf")
-	telescope.load_extension("file_browser")
+telescope.load_extension("fzf")
+telescope.load_extension("file_browser")
 
-	search_dotfiles = function()
-		telescope.extensions.file_browser.file_browser({
-			prompt_title = "NeoVimRC",
-			cwd = vim.fn.stdpath("config"),
-		})
-	end
-
-	browse_blog = function()
-		telescope.extensions.file_browser.file_browser({
-			prompt_title = "Blog",
-			cwd = "~/code/mactep.github.io/_posts",
-		})
-	end
-
-	find_wiki = function()
-		require("telescope.builtin").find_files({
-			prompt_title = "Find Wiki",
-			cwd = "~/Dropbox/wiki",
-			attach_mappings = function(prompt_bufnr, map)
-				map("i", "<CR>", function(prompt_bufnr)
-					-- filename is available at entry[1]
-					local entry = require("telescope.actions.state").get_selected_entry()
-					require("telescope.actions").close(prompt_bufnr)
-					local filename = entry[1]
-					-- Insert filename in current cursor position
-					vim.cmd("normal i[" .. filename .. "](" .. filename .. ")")
-				end)
-
-				return true
-			end,
-		})
-	end
-
-	vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<leader>fi", "<cmd>Telescope find_files no_ignore=true<cr>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<leader>fr", "<cmd>Telescope live_grep<cr>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<C-n>", "<cmd>Telescope file_browser<cr>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<leader>fB", "<cmd>Telescope buffers<cr>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<leader>fv", "<cmd>lua search_dotfiles()<cr>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua browse_blog()<cr>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<leader>fw", "<cmd>lua find_wiki()<cr>", { noremap = true })
+local find_files_hidden = function()
+	telescope.builtin.find_files({ no_ignore = true })
 end
+
+local search_dotfiles = function()
+	telescope.extensions.file_browser.file_browser({
+		prompt_title = "NeoVimRC",
+		cwd = vim.fn.stdpath("config"),
+	})
+end
+
+local browse_blog = function()
+	telescope.extensions.file_browser.file_browser({
+		prompt_title = "Blog",
+		cwd = "~/code/mactep.github.io/_posts",
+	})
+end
+
+local find_wiki = function()
+	telescope.builtin.find_files({
+		prompt_title = "Find Wiki",
+		cwd = "~/Dropbox/wiki",
+		attach_mappings = function(_, map)
+			map("i", "<CR>", function(prompt_bufnr)
+				-- filename is available at entry[1]
+				local entry = require("telescope.actions.state").get_selected_entry()
+				require("telescope.actions").close(prompt_bufnr)
+				local filename = entry[1]
+				-- Insert filename in current cursor position
+				vim.cmd("normal i[" .. filename .. "](" .. filename .. ")")
+			end)
+
+			return true
+		end,
+	})
+end
+
+local explore_current_file_dir = function()
+	local path = vim.fn.expand("%:p:h")
+	telescope.extensions.file_browser.file_browser({
+		prompt_title = "Explore current file directory",
+		cwd = path,
+	})
+end
+
+vim.keymap.set("n", "<leader>ff", telescope.builtin.find_files, { noremap = true })
+vim.keymap.set("n", "<leader>fi", find_files_hidden, { noremap = true })
+vim.keymap.set("n", "<leader>fr", telescope.builtin.live_grep, { noremap = true })
+vim.keymap.set("n", "<leader>fh", telescope.builtin.help_tags, { noremap = true })
+vim.keymap.set("n", "<leader>fB", telescope.builtin.buffers, { noremap = true })
+vim.keymap.set("n", "<C-n>", telescope.extensions.file_browser.file_browser, { noremap = true })
+vim.keymap.set("n", "<A-n>", explore_current_file_dir, { noremap = true })
+vim.keymap.set("n", "<leader>fv", search_dotfiles, { noremap = true })
+vim.keymap.set("n", "<leader>fb", browse_blog, { noremap = true })
+vim.keymap.set("n", "<leader>fw", find_wiki, { noremap = true })
