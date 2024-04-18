b95d9d39e9fec7608d909651bdc4c489fd03ec4a
mactep <brunoro.tulio@gmail.com>
2024-04-18 10:50:55 -0300

    nixos: update flakes

--- .config/flake.nix
+++ .config/flake.nix
@@ -2,9 +2,12 @@
   description = "Home Manager and NixOS configuration of mactep";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -16,33 +19,41 @@
       url = "github:rafaelmardojai/firefox-gnome-theme";
       flake = false;
     };
+
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-  let
-    # system settings
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+    let
+      # system settings
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [ inputs.neovim-nightly-overlay.overlay ];
+      };
 
-    # user settings
-    user = {
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+      };
+
+      # user settings
+      user = {
         name = "mactep";
         email = "brunoro.tulio@gmail.com";
-    };
-  in
-  {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs system user; };
-      modules = [ ./nixos/configuration.nix ];
-    };
+        shell = "fish";
+      };
+    in
+    {
+      nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs system user; };
+        modules = [ ./nixos/configuration.nix ];
+      };
 
-    homeConfigurations."mactep" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit inputs user; };
-      modules = [ ./home-manager/home.nix ];
+      homeConfigurations.${user.name} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs user pkgs-unstable; };
+        modules = [ ./home-manager/home.nix ];
+      };
     };
-  };
 }
